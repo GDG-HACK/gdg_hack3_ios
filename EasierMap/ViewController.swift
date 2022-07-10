a3ef724486c400c2960f2b21c11ce23fd4927a81
heydoy <heydoyhey@gmail.com>
2022-07-11 00:21:23 +0900

    Initial Commit

---
+++ EasierMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasierMap
+//
+//  Created by Doy Kim on 2022/07/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
